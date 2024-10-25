63ae0e54c751449e2c29ba2d5f7026723226eacb
Vijay <vijaykumarche68@gmail.com>
2024-10-25 17:01:54 +0530

    initial commit

---
+++ src/main/java/com/practice/Sbms1Application.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbms1Application {
+
+	public static void main(String[] args) {
+		 SpringApplication.run(Sbms1Application.class, args);
+		System.out.println("hii");
+
+}}
---
+++ src/main/java/com/practice/entity/Employee.java
@@ -0,0 +1,32 @@
+package com.practice.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Employee {
+	
+	private Integer empid;
+	private String empname;
+	private double empsalary;
+	public Integer getEmpid() {
+		return empid;
+	}
+	public void setEmpid(Integer empid) {
+		this.empid = empid;
+	}
+	public String getEmpname() {
+		return empname;
+	}
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+	public double getEmpsalary() {
+		return empsalary;
+	}
+	public void setEmpsalary(double empsalary) {
+		this.empsalary = empsalary;
+	}
+	
+	
+
+}
